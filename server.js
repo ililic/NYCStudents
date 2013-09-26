d0dbf369d99e2fc53198368530f86f3cfe0c7736
ililic <Thoughtworker@192.168.1.6>
2013-09-25 22:50:26 -0400

    Added index and server js for simple node app

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var buffer = fs.readFileSync('index.html');
+var hello = buffer.toString();
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(hello);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
